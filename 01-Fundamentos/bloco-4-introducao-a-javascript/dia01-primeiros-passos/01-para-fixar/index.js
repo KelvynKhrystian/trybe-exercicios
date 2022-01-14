3265e3f972ebc99d9bfe18c68803ab545239804f
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-01-14 17:10:46 -0300

    parte1-ok

---
+++ 01-Fundamentos/bloco-4-introducao-a-javascript/dia01-primeiros-passos/01-para-fixar/index.js
@@ -0,0 +1,9 @@
+let myName = "KelvynKhrystian";
+const birthCity = "saoLuis";
+let birthYear = 1996;
+
+console.log(myName, birthCity, birthYear);
+birthYear =2030;
+console.log(birthYear);
+birthCity ="Islandia"; // deu erro pois uma constante não se pode alterar o valor
+console.log(birthCity);
