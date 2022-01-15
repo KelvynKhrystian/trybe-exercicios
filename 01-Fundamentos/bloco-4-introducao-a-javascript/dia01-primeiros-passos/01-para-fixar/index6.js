ce63dd8925192aa0b88ad88aeb9b31f183ea3949
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-01-15 02:59:36 -0300

    parte6-ok

---
+++ 01-Fundamentos/bloco-4-introducao-a-javascript/dia01-primeiros-passos/01-para-fixar/index6.js
@@ -0,0 +1,15 @@
+let processo = "aprovadaa";
+
+switch (processo){
+    case "aprovada":
+        console.log("aprovada");
+        break;
+    case "reprovada":
+        console.log("reprovada");
+        break;
+    case "lista":
+        console.log("lista");
+        break;
+    default:
+        console.log("não se aplica")
+}
