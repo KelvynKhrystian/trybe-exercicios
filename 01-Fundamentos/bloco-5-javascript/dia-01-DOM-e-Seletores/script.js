bcc7a7745c1e4a7466053208c433964cd359ee96
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-01-25 23:47:54 -0300

    Ok

---
+++ 01-Fundamentos/bloco-5-javascript/dia-01-DOM-e-Seletores/script.js
@@ -0,0 +1,17 @@
+document.querySelector("h1").style.backgroundColor = "green"; 
+document.querySelector("h1").style.padding = "25px"; 
+
+document.querySelectorAll(".emergency-tasks div h3")[0].style.backgroundColor = "purple";
+document.querySelectorAll(".emergency-tasks div h3")[1].style.backgroundColor = "purple";
+
+document.querySelectorAll(".emergency-tasks div")[0].style.backgroundColor = "pink";
+document.querySelectorAll(".emergency-tasks div")[1].style.backgroundColor = "pink";
+
+document.querySelectorAll(".no-emergency-tasks div h3")[0].style.backgroundColor = "black";
+document.querySelectorAll(".no-emergency-tasks div h3")[1].style.backgroundColor = "black";
+
+document.querySelectorAll(".no-emergency-tasks div")[0].style.backgroundColor = "yellow";
+document.querySelectorAll(".no-emergency-tasks div")[1].style.backgroundColor = "yellow";
+
+document.querySelector("footer").style.backgroundColor = "green"; 
+document.querySelectorAll("#footer-container div")[0].style.backgroundColor = "green"; 
