98e5ef126138e7e4b4788a6d6d6c60369bf361b6
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-02-15 13:53:43 -0300

    calendario

--- 01-Fundamentos/bloco-5-javascript/dia-03-Eventos/exercicios/script.js
+++ 01-Fundamentos/bloco-5-javascript/dia-03-Eventos/exercicios/script.js
@@ -111,6 +111,25 @@ function criarSexta(string){
 
 criarSexta("Sexta-feira");
 
+function modificarSexta() {
+    let getHolidayButton = document.querySelector('#btn-holiday');
+    let getHolidays = document.querySelectorAll('.holiday')
+    let backgroundColor = 'rgb(238,238,238)';
+    let setNewColor = 'white';
+  
+    getHolidayButton.addEventListener('click', function() {
+      for (let index = 0; index < getHolidays.length; index += 1) {
+        if (getHolidays[index].style.backgroundColor === setNewColor) {
+          getHolidays[index].style.backgroundColor = backgroundColor;
+        } else {
+          getHolidays[index].style.backgroundColor = setNewColor;
+        }
+      }
+    })
+  };
+  
+  displayHolidays();
+
 
 
 
