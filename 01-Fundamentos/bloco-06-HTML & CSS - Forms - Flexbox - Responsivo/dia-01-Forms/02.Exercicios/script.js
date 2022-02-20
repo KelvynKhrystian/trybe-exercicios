adfc777fb0408cd93b164f4da0a6808def42181b
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-02-20 04:06:33 -0300

    forms-quase-ok

--- 01-Fundamentos/bloco-06-HTML & CSS - Forms - Flexbox - Responsivo/dia-01-Forms/02.Exercicios/script.js
+++ 01-Fundamentos/bloco-06-HTML & CSS - Forms - Flexbox - Responsivo/dia-01-Forms/02.Exercicios/script.js
@@ -1,11 +1,28 @@
+window.onload = function() {
+    const apagar = document.getElementById("apagar");
+    apagar.addEventListener('click', apagar());
+    const button = document.getElementById("enviar");
+    button.addEventListener("click", enviar());
+    
+}
+
 function enviar(event){
     event.preventDefault();
 }
 
-window.onload = function() {
-    const button = document.getElementById("enviar");
-    button.addEventListener("click", enviar());
+function apagar() {
+    const formElements = document.querySelectorAll('input');
+    const textArea = document.querySelector('textarea');
+    for (let index = 0; index < formElements.length; index += 1) {
+      const userInput = formElements[index];
+      userInput.value = '';
+      userInput.checked = false;
+    }
+    textArea.value = '';
+   
 }
+
+
 
 
 
