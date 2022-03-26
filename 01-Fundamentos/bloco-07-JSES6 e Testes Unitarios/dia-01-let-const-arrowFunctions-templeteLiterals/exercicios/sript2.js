af725f66acb856ba2ace0f6672daacdd3692996d
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-03-25 22:38:37 -0300

    ok-revisando

--- 01-Fundamentos/bloco-07-JSES6 e Testes Unitarios/dia-01-let-const-arrowFunctions-templeteLiterals/exercicios/sript2.js
+++ 01-Fundamentos/bloco-07-JSES6 e Testes Unitarios/dia-01-let-const-arrowFunctions-templeteLiterals/exercicios/sript2.js
@@ -1,9 +1,6 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-const sortArrayBonus = array => {
-  const sortOddsAndEvens = array.sort((a, b) => a - b);
-  return sortOddsAndEvens;
-}
+const sortArrayBonus = array => array.sort((a, b) => a - b);
 
 const sortedArrayBonus = sortArrayBonus(oddsAndEvens);
 console.log(`Os números ${sortedArrayBonus} se encontram ordenados de forma crescente !`);
