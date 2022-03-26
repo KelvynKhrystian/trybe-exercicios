bfc8d1caa1cc8228f8432214c89ad103a4a264b0
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-03-26 03:11:32 -0300

    rev

--- 01-Fundamentos/bloco-07-JSES6 e Testes Unitarios/dia-02-fluxo de exccessão e objetos/objectEntreis.js
+++ 01-Fundamentos/bloco-07-JSES6 e Testes Unitarios/dia-02-fluxo de exccessão e objetos/objectEntreis.js
@@ -3,12 +3,12 @@ const países = {
     Brasil: 'Brasília',
     Espanha: 'Madrid',
     Portugal: 'Lisboa',
-  };
-  const pairKeyValue = Object.entries(países);
-  console.log(pairKeyValue);
+};
+const pairKeyValue = Object.entries(países);
+    console.log(pairKeyValue);
 
-  for(index in pairKeyValue) {
+for(index in pairKeyValue) {
     console.log('--------');
     console.log('País:', pairKeyValue[index][0]);
     console.log('Capital:', pairKeyValue[index][1]);
-  };
+};
