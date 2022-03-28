cd2bd7c06f39a00f2b472344f5687d84533c0273
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-03-28 02:05:16 -0300

    ok

--- 01-Fundamentos/bloco-07-JSES6 e Testes Unitarios/dia-02-fluxo de exccessão e objetos/objectAssign.js
+++ 01-Fundamentos/bloco-07-JSES6 e Testes Unitarios/dia-02-fluxo de exccessão e objetos/objectAssign.js
@@ -10,3 +10,17 @@ const newPerson = Object.assign({},person,lastName);
 newPerson.name = 'Gilberto';
 console.log(newPerson);
 console.log(person);
+
+
+// const person = {
+//     name: 'Roberto',
+//   };
+  
+//   const lastName = {
+//     lastName: 'Silva',
+//   };
+  
+//   const clone = Object.assign(person, lastName);
+  
+//   console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+//   console.log(person); // { name: 'Roberto', lastName: 'Silva' }
