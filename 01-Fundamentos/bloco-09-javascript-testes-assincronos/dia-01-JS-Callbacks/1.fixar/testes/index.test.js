17596f924d8fd02a4b09081ce7ca655f44ad31be
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-04-14 17:53:55 -0300

    api

--- 01-Fundamentos/bloco-09-javascript-testes-assincronos/dia-01-JS-Callbacks/1.fixar/testes/index.test.js
+++ 01-Fundamentos/bloco-09-javascript-testes-assincronos/dia-01-JS-Callbacks/1.fixar/testes/index.test.js
@@ -1,6 +1,17 @@
-test('Não deveria passar!', () => {
-    setTimeout(() => {
-      expect(10).toBe(5);
-      console.log('Deveria falhar!');
-    }, 500);
+const asyncSum = (a, b, callback) => {
+  setTimeout(() => {
+    const result = a + b;
+    callback(result);
+  }, 500);
+};
+
+test('Testando asyncSum, soma 5 mais 10', (done) => {
+  asyncSum(5, 10, (result) => {
+    try {
+      expect(result).toBe(15);
+      done();
+    } catch (error) {
+      done(error);
+    }
   });
+});
