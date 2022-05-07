7ff2f46f15b110b669903d3099301ecdcd87e21c
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-05-07 04:31:45 -0300

    exercicios

---
+++ 02-FrontEnd/bloco-10-introdução-a-React/dia-01-hello-world/exercicio/exercicio.01.02/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  const Task = (value) => {
+    return (
+      <li>{value}</li>
+    );
+  }
+
+  return (
+    <div>
+      testando
+    </div>
+  );
+}
+
+export default App;
---
+++ 02-FrontEnd/bloco-10-introdução-a-React/dia-01-hello-world/exercicio/fixar/fixar2/src/Component.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Kelvyn Khrystian</h1>
+        <p>Uma pessoa aleatória se esforçando para mudar</p>
+      </div>
+            
+    )
+  }
+}
+
+export default Component;
