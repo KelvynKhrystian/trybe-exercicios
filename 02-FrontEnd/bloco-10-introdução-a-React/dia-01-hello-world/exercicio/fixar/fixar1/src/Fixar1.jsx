a9905d5572a779fead2424a5e6a5482309cb9b44
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-05-07 03:58:23 -0300

    react-fixar

---
+++ 02-FrontEnd/bloco-10-introdução-a-React/dia-01-hello-world/exercicio/fixar/fixar1/src/Fixar1.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const textJSX = <h1> Hello, JSX</h1>
+
+class Fixar1 extends React.Component {
+
+    render() {
+        return (
+            textJSX
+        );
+    }
+}
+
+export default Fixar1;
