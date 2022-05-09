7404b1a7668ea96eb53c3d5526bc0559c72686b4
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-05-08 21:42:45 -0300

    tudo

--- 02-FrontEnd/bloco-10-introdução-a-React/dia-01-hello-world/exercicio/exercicio.01.02/src/App.js
+++ 02-FrontEnd/bloco-10-introdução-a-React/dia-01-hello-world/exercicio/exercicio.01.02/src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import './App.css';
 
-function App() {
+    import React from 'react';
+    import './App.css';
 
-  const Task = (value) => {
-    return (
-      <li>{value}</li>
-    );
-  }
+    const Task = (value) => {
+      return (
+        <li key={value}>{value}</li>
+      );
+    }
+    
+    const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
 
-  return (
-    <div>
-      testando
-    </div>
-  );
-}
+    class App extends React.Component {
+      render() {
+        return (
+          <ul>{ tarefas.map(tarefa => Task(tarefa)) }</ul>
+        );
+      }
+    }
 
-export default App;
+    export default App;
