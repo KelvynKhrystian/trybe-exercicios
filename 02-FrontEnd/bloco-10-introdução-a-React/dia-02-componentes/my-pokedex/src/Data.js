00375a8c0e73d7faa5f8b99fe2862cb93b8f6394
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-05-09 00:13:53 -0300

    pokedex ok

--- 02-FrontEnd/bloco-10-introdução-a-React/dia-02-componentes/my-pokedex/src/Data.js
+++ 02-FrontEnd/bloco-10-introdução-a-React/dia-02-componentes/my-pokedex/src/Data.js
@@ -1,14 +1,14 @@
 const pokemons = [
   {
-      id: 25,
-      name: "Pikachu",
-      type: 'Electric',
-      averageWeight: {
-          value: 6.0,
-          measurementUnit: "kg"
-      },
-      image: "https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png",
-      moreInfo: "https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)"
+    id: 25,
+    name: "Pikachu",
+    type: 'Electric',
+    averageWeight: {
+      value: 6.0,
+      measurementUnit: "kg"
+    },
+    image: "https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png",
+    moreInfo: "https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)"
   },
   {
       id: 4,
