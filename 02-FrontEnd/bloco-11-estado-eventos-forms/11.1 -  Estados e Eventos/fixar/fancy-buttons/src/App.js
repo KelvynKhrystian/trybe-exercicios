49e5a07457121139d05628dc6916c5a9eb0a8078
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-05-14 05:33:42 -0300

    fixar.05.05.07

--- 02-FrontEnd/bloco-11-estado-eventos-forms/11.1 -  Estados e Eventos/fixar/fancy-buttons/src/App.js
+++ 02-FrontEnd/bloco-11-estado-eventos-forms/11.1 -  Estados e Eventos/fixar/fancy-buttons/src/App.js
@@ -8,28 +8,40 @@ class App extends React.Component {
   constructor() {
     super()
 
+    this.state = {
+      btn1Click : 0 ,
+      btn2Click : 0 ,
+      btn3Click : 0
+    }
+
     this.btn1 = this.btn1.bind(this)
     this.btn2 = this.btn2.bind(this)
     this.btn3 = this.btn3.bind(this)
   }
 
   btn1() {
-    console.log('Clicou no 1 !', this)
+    this.setState ((estadoAnterior, _props) => ({
+      btn1Click : estadoAnterior.btn1Click + 1
+    }))
   }
   
   btn2() {
-    console.log('Clicou no 2 !')
+    this.setState ((estadoAnterior, _props) => ({
+      btn2Click : estadoAnterior.btn2Click + 1
+    }))
   }
   
   btn3() {
-    console.log('Clicou no 3 !')
+    this.setState ((estadoAnterior, _props) => ({
+      btn3Click : estadoAnterior.btn3Click + 1
+    }))
   }
   render() {
     return (
       <div>
-          <button onClick={this.btn1}>Botão-1</button>
-          <button onClick={this.btn2}>Botão-2</button>
-          <button onClick={this.btn3}>Botão-3</button>
+          <button onClick={this.btn1}>Botão "1" :  {this.state.btn1Click} </button>
+          <button onClick={this.btn2}>{this.state.btn2Click}</button>
+          <button onClick={this.btn3}>{this.state.btn3Click}</button>
       </div>
     );
   }
