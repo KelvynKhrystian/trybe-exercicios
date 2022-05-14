a7f19af4146f55274944f42f8e760c2074e47185
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-05-14 04:32:14 -0300

    fixar.03.04

--- 02-FrontEnd/bloco-11-estado-eventos-forms/11.1 -  Estados e Eventos/fixar/fancy-buttons/src/App.js
+++ 02-FrontEnd/bloco-11-estado-eventos-forms/11.1 -  Estados e Eventos/fixar/fancy-buttons/src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import './App.css';
 
-function btn1() {
-  console.log('Clicou no 1 !')
-}
 
-function btn2() {
-  console.log('Clicou no 2 !')
-}
-
-function btn3() {
-  console.log('Clicou no 3 !')
-}
 
 class App extends React.Component {
+
+  constructor() {
+    super()
+
+    this.btn1 = this.btn1.bind(this)
+    this.btn2 = this.btn2.bind(this)
+    this.btn3 = this.btn3.bind(this)
+  }
+
+  btn1() {
+    console.log('Clicou no 1 !', this)
+  }
+  
+  btn2() {
+    console.log('Clicou no 2 !')
+  }
+  
+  btn3() {
+    console.log('Clicou no 3 !')
+  }
   render() {
     return (
       <div>
-          <button onClick={btn1}>Botão-1</button>
-          <button onClick={btn2}>Botão-2</button>
-          <button onClick={btn3}>Botão-3</button>
+          <button onClick={this.btn1}>Botão-1</button>
+          <button onClick={this.btn2}>Botão-2</button>
+          <button onClick={this.btn3}>Botão-3</button>
       </div>
     );
   }
