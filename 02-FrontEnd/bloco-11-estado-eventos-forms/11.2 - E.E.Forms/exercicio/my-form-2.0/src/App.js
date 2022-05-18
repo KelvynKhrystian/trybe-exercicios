812d1e2cd8844438cfcf1ff13576d8af00b0ceb6
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-05-18 04:43:48 -0300

    ok

---
+++ 02-FrontEnd/bloco-11-estado-eventos-forms/11.2 - E.E.Forms/exercicio/my-form-2.0/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Forms1 from './Forms1';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Exercício React Forms</h1>
+        <Forms1 />
+      </div>
+    );
+  }
+  
+}
+
+export default App;
