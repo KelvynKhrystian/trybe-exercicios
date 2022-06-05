28853a2c5fee7092d29d87ac4be9eb03b3394627
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-06-05 00:49:56 -0300

    fixar.02

--- 02-FrontEnd/bloco-11-estado-eventos-forms/11.1 -  Estados e Eventos/fixar/fixar01/src/App.js
+++ 02-FrontEnd/bloco-11-estado-eventos-forms/11.1 -  Estados e Eventos/fixar/fixar01/src/App.js
@@ -4,10 +4,23 @@ function fixar() {
   console.log('Clicou no botão!')
 }
 
+function fixara() {
+  console.log('Clicou no botão!aaaaaaaaaaaaa')
+}
+
+function fixarb() {
+  console.log('Clicou no botão!bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
+}
+
 class App extends React.Component {
   render() {
     return (
-      <button onClick={fixar}>Botão Fixar</button>
+      <>
+        <button onClick={fixar}>Botão Fixar</button>
+        <button onClick={fixara}>Botão Fixar2</button>
+        <button onClick={fixarb}>Botão Fixar3</button>
+      </>
+      
     );
   }
 }
