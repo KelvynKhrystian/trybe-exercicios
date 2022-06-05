0d3c954fcad07968d367b31948a20fcdf09d85ad
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-06-05 01:02:35 -0300

    fixar 03.04

--- 02-FrontEnd/bloco-11-estado-eventos-forms/11.1 -  Estados e Eventos/fixar/fixar01/src/App.js
+++ 02-FrontEnd/bloco-11-estado-eventos-forms/11.1 -  Estados e Eventos/fixar/fixar01/src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
 
-function fixar() {
-  console.log('Clicou no botão!')
-}
-
-function fixara() {
-  console.log('Clicou no botão!aaaaaaaaaaaaa')
-}
-
-function fixarb() {
-  console.log('Clicou no botão!bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
-}
-
 class App extends React.Component {
+  
+  constructor() {
+    super()
+    this.fixar = this.fixar.bind(this);
+    this.fixar2 = this.fixar2.bind(this);
+    this.fixar3 = this.fixar3.bind(this);
+  }
+
+  fixar() {
+      console.log('Clicou no botão! 1')
+  }
+    
+  fixar2() {
+      console.log('Clicou no botão! 2')
+  }
+    
+  fixar3() {
+      console.log('Clicou no botão! 3')
+  }
+
   render() {
     return (
       <>
-        <button onClick={fixar}>Botão Fixar</button>
-        <button onClick={fixara}>Botão Fixar2</button>
-        <button onClick={fixarb}>Botão Fixar3</button>
+        <button onClick={this.fixar}>Botão Fixar</button>
+        <button onClick={this.fixar2}>Botão Fixar2</button>
+        <button onClick={this.fixar3}>Botão Fixar3</button>
       </>
       
     );
