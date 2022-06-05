fe0adbe52e8cbc76a0d19692254712a8dfa68829
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-06-05 02:43:35 -0300

    fixar.5.6.7

--- 02-FrontEnd/bloco-11-estado-eventos-forms/11.1 -  Estados e Eventos/fixar/fixar01/src/App.js
+++ 02-FrontEnd/bloco-11-estado-eventos-forms/11.1 -  Estados e Eventos/fixar/fixar01/src/App.js
@@ -1,32 +1,46 @@
 import React from 'react';
 
 class App extends React.Component {
-  
+
   constructor() {
     super()
     this.fixar = this.fixar.bind(this);
     this.fixar2 = this.fixar2.bind(this);
     this.fixar3 = this.fixar3.bind(this);
+    this.state = {
+      btn1: 0,
+      btn2: 0,
+      btn3: 0,
+    }
   }
 
   fixar() {
-      console.log('Clicou no botão! 1')
-  }
+    console.log('Clicou no botão! 1')
+    this.setState((estadoAnterior, _props) => ({
+      btn1: estadoAnterior.btn1 + 1,
+    }));
+  };
     
   fixar2() {
       console.log('Clicou no botão! 2')
-  }
+      this.setState ((estadoAnterior, _props) => ({
+        btn2: estadoAnterior.btn2 + 1,
+      }));
+  };
     
   fixar3() {
       console.log('Clicou no botão! 3')
+      this.setState((estadoAnterior, _props) => ({
+        btn3: estadoAnterior.btn3 + 1,
+      }))
   }
 
   render() {
     return (
       <>
-        <button onClick={this.fixar}>Botão Fixar</button>
-        <button onClick={this.fixar2}>Botão Fixar2</button>
-        <button onClick={this.fixar3}>Botão Fixar3</button>
+        <button onClick={this.fixar}>{this.state.btn1}</button>
+        <button onClick={this.fixar2}>{this.state.btn2}</button>
+        <button onClick={this.fixar3}>{this.state.btn3}</button>
       </>
       
     );
