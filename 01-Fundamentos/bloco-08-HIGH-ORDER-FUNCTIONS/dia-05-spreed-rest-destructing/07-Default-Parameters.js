e9cd22beed4a55c237511b6cf651a8ace259c55d
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-06-18 05:48:11 -0300

    default-parameter

---
+++ 01-Fundamentos/bloco-08-HIGH-ORDER-FUNCTIONS/dia-05-spreed-rest-destructing/07-Default-Parameters.js
@@ -0,0 +1,9 @@
+const multiply = (number = 1, value = 1) => {
+  // Escreva aqui a sua função
+  return number * value
+};
+  
+console.log(multiply(8,10));
+console.log(multiply(8,));
+console.log(multiply());
+console.log(multiply(10));
