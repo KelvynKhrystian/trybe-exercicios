788d16486d515cee61f912fcdea9512c0be46257
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-11-25 15:54:05 -0300

    exercicio1-testes

--- 01-Fundamentos/bloco-07-JSES6 e Testes Unitarios/revision/myRemove.test.js
+++ 01-Fundamentos/bloco-07-JSES6 e Testes Unitarios/revision/myRemove.test.js
@@ -12,14 +12,19 @@ function myRemove(arr, item) {
 
 describe ('🚀 Exercício 1 - A função myRemove()', () => {
 
-  it ('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado;', () => {
-
+  it ('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array [1, 2, 4]', () => {
+    const arrayTest = [1, 2, 3, 4]
+    expect([1, 2, 4]).toEqual(myRemove(arrayTest, 3))
   });
+
   it ('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4];', () => {
-
+    const arrayTest = [1, 2, 3, 4]
+    expect(myRemove(arrayTest, 3)).not.toContain(3)
   });
-  it ('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado.', () => {
 
+  it ('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado.', () => {
+    const arrayTest = [1, 2, 3, 4]
+    expect([1, 2, 3, 4]).toEqual(myRemove(arrayTest, 5))
   });
 });
 
