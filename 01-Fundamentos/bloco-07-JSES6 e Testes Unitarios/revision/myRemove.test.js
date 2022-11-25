397adcde47b5ab1970fad996493a9ca2563a69b5
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-11-25 02:30:21 -0300

    pausa-jest

--- 01-Fundamentos/bloco-07-JSES6 e Testes Unitarios/revision/myRemove.test.js
+++ 01-Fundamentos/bloco-07-JSES6 e Testes Unitarios/revision/myRemove.test.js
@@ -1,11 +1,31 @@
 function myRemove(arr, item) {
-    let newArr = [];
-    for (let index = 0; index < arr.length; index += 1) {
-      if (item !== arr[index]) {
-        newArr.push(arr[index]);
-      }
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
     }
-    return newArr;
   }
+  return newArr;
+}
   
-  // implemente seus testes aqui
+// implemente seus testes aqui
+
+describe ('🚀 Exercício 1 - A função myRemove()', () => {
+
+  it ('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado;', () => {
+
+  });
+  it ('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4];', () => {
+
+  });
+  it ('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado.', () => {
+
+  });
+});
+
+describe ('🚀 Exercício 2 - A função myFizzBuzz(num)', () => {
+
+  it ('A função myRemove', () => {
+
+  });
+});
