6193e42583adb20d9daabf38180dba506e769fed
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-11-26 01:42:39 -0300

    inciando-exercicio-1

---
+++ 01-Fundamentos/bloco-07-JSES6 e Testes Unitarios/exercicios-testes/sum.test.js
@@ -0,0 +1,9 @@
+const sum = require('./sum.js');
+
+
+describe ('exercicio 1', () => {
+    it.todo('Teste se o retorno de sum(4, 5) é 9');
+    it.todo('Teste se o retorno de sum(0, 0) é 0.');
+    it.todo('Teste se a função sum lança um erro quando os parâmetros são number 4 e string 5');
+    it.todo('Teste se a mensagem de erro é parameters must be numbers quando realizar a chamada sum(4,5)');
+})
