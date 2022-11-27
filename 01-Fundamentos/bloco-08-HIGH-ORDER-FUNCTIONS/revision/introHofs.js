33720384b3923bfa7bcf059df28c2b0d9de4b428
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-11-27 16:01:19 -0300

    fixar-every

--- 01-Fundamentos/bloco-08-HIGH-ORDER-FUNCTIONS/revision/introHofs.js
+++ 01-Fundamentos/bloco-08-HIGH-ORDER-FUNCTIONS/revision/introHofs.js
@@ -42,6 +42,21 @@ const hasName = (arr, name) => {
   return arr.some((element) => element === name)
 };
 
-console.log(hasName(names2, 'Ana'));
-console.log(hasName(names2, 'Pedro'));
+// console.log(hasName(names2, 'Ana'));
+// console.log(hasName(names2, 'Pedro'));
 
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+//Adicione seu código aqui
+const verifyAges = (arr,idade) => {
+  return arr.every((element) => element >= idade)
+}
+
+// console.log(verifyAges(people, 18));
+// console.log(verifyAges(people, 14));
