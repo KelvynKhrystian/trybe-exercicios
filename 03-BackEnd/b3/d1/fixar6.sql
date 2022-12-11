0c582dd29d44be8eef07de3b6bfac66b15b5743f
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-11 00:00:03 -0300

    fixar 6 parte 2 git push

--- 03-BackEnd/b3/d1/fixar6.sql
+++ 03-BackEnd/b3/d1/fixar6.sql
@@ -34,4 +34,27 @@ ORDER BY avg_rental_duration DESC;
 SELECT district, COUNT(address) AS 'enderecos'
 FROM sakila.address
 GROUP BY district
-ORDER BY 'enderecos' DESC
+ORDER BY 'enderecos' DESC;
+
+
+-- Usando a query a seguir, modifique-a de forma que exiba apenas as 
+-- durações médias que estão entre 115.0 a 121.50. Além disso, dê um 
+-- alias (apelido) à coluna gerada por AVG(length), de forma que deixe 
+-- a query mais legível. Finalize ordenando os resultados de forma decrescente.
+
+SELECT rating, AVG(length) AS media
+FROM sakila.film
+GROUP BY rating
+HAVING media BETWEEN 115.0 AND 121.50
+ORDER BY media DESC;
+
+
+-- Usando a query a seguir, exiba apenas os valores de total do custo de substituição 
+-- que estão acima de $3950.50. Dê um alias que faça sentido para SUM(replacement_cost), 
+-- de forma que deixe a query mais legível. Finalize ordenando os resultados de forma crescente.
+
+SELECT rating, SUM(replacement_cost) AS soma_substituicao
+FROM sakila.film
+GROUP by rating
+HAVING soma_substituicao > 3950.5
+ORDER BY soma_substituicao ASC;
