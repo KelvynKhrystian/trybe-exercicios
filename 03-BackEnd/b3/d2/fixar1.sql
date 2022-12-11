fe863271463c3975f83dc5312320ef1c0b7d9a95
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-11 16:18:05 -0300

    fixar1-q4

--- 03-BackEnd/b3/d2/fixar1.sql
+++ 03-BackEnd/b3/d2/fixar1.sql
@@ -15,9 +15,6 @@ FROM sakila.staff AS s
 INNER JOIN sakila.address AS a
 ON s.address_id = a.address_id;
 
-
-
-
 -- Exiba o id do cliente, nome e email dos primeiros 100 clientes, 
 -- ordenados pelo nome em ordem decrescente, juntamente com o id do endereço 
 -- e o nome da rua onde o cliente mora. Essas informações podem ser encontradas 
@@ -29,3 +26,14 @@ INNER JOIN sakila.address AS a
 ON c.address_id = a.address_id
 ORDER BY first_name DESC
 LIMIT 100;
+
+-- Exiba o nome, email, id do endereço, endereço e distrito dos clientes 
+-- que moram no distrito da California e que contêm “rene” em seus nomes. 
+-- As informações podem ser encontradas nas tabelas address e customer.
+
+SELECT c.first_name, c.email, c.address_id, a.address, a.district
+FROM sakila.customer AS c
+INNER JOIN sakila.address AS a
+ON c.address_id = a.address_id
+WHERE a.district = 'California'AND c.first_name LIKE '%rene%';
+
