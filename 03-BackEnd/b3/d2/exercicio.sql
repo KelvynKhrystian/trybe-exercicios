54b16316b99f35a8e09d54791c0230354731d23c
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-11 18:35:05 -0300

    exercicio-q3

--- 03-BackEnd/b3/d2/exercicio.sql
+++ 03-BackEnd/b3/d2/exercicio.sql
@@ -17,6 +17,11 @@ WHERE b.international_sales > b.domestic_sales;
 -- 🚀 Exercício 3: Utilizando o INNER JOIN, faça uma busca que retorne os filmes 
 -- e sua avaliação (rating) em ordem decrescente.
 
+SELECT m.title, b.rating
+FROM movies m
+INNER JOIN box_office b ON b.movie_id = m.id
+ORDER BY b.rating DESC;
+
 -- Exercício 4: Utilizando o LEFT JOIN, faça uma busca que retorne todos os dados 
 -- dos cinemas, mesmo os que não possuem filmes em cartaz e, adicionalmente, os dados
 -- dos filmes que estão em cartaz nestes cinemas. Retorne os nomes dos cinemas em ordem alfabética.
