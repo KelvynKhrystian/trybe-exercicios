528885265809f2b48e0ec7ae3d44be9580ca7d7e
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-11 15:59:50 -0300

    fixar1-q2

--- 03-BackEnd/b3/d2/fixar1.sql
+++ 03-BackEnd/b3/d2/fixar1.sql
@@ -7,3 +7,13 @@ FROM sakila.actor AS a
 INNER JOIN sakila.film_actor AS b
 ON a.actor_id = b.film_id;
 
+-- Use o JOIN para exibir o nome, sobrenome e endereço de cada um dos 
+-- funcionários do banco. Use as tabelas staff e address.
+
+SELECT CONCAT(s.first_name,' ',s.last_name) AS full_name, a.address
+FROM sakila.staff AS s
+INNER JOIN sakila.address AS a
+ON s.address_id = a.address_id;
+
+
+
