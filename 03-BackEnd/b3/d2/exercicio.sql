1b5f2ae3d1170cc3661ba59d6d79c0c7bf2741c2
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-11 18:47:19 -0300

    exercicio-q4

--- 03-BackEnd/b3/d2/exercicio.sql
+++ 03-BackEnd/b3/d2/exercicio.sql
@@ -2,7 +2,7 @@
 -- e internacionais (international_sales) de cada filme.
 
 SELECT m.title, b.domestic_sales, b.international_sales
-FROM movies m
+FROM movies AS m
 INNER JOIN box_office b ON b.movie_id = m.id;
 
 -- 🚀 Exercício 2: Utilizando o INNER JOIN, faça uma busca que retorne o número de 
@@ -10,7 +10,7 @@ INNER JOIN box_office b ON b.movie_id = m.id;
 -- (international_sales) do que vendas nacionais (domestic_sales).
 
 SELECT m.title, (b.domestic_sales + b.international_sales) AS "Vendas"
-FROM movies m
+FROM movies AS m
 INNER JOIN box_office b ON b.movie_id = m.id
 WHERE b.international_sales > b.domestic_sales;
 
@@ -18,13 +18,18 @@ WHERE b.international_sales > b.domestic_sales;
 -- e sua avaliação (rating) em ordem decrescente.
 
 SELECT m.title, b.rating
-FROM movies m
+FROM movies AS m
 INNER JOIN box_office b ON b.movie_id = m.id
 ORDER BY b.rating DESC;
 
 -- Exercício 4: Utilizando o LEFT JOIN, faça uma busca que retorne todos os dados 
 -- dos cinemas, mesmo os que não possuem filmes em cartaz e, adicionalmente, os dados
 -- dos filmes que estão em cartaz nestes cinemas. Retorne os nomes dos cinemas em ordem alfabética.
+
+SELECT t.id, t.name, t.location, m.title, m.director, m.year, m.length_minutes
+FROM theater AS t
+LEFT JOIN movies AS m ON t.id = m.theater_id
+ORDER BY t.name ASC;
 
 -- Exercício 5: Utilizando o RIGHT JOIN, faça uma busca que retorne todos os dados dos filmes, 
 -- mesmo os que não estão em cartaz e, adicionalmente, os dados dos cinemas que possuem estes 
