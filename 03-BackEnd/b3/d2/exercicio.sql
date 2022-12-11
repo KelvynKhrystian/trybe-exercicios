d174fe420b5ca700875ef02049350ae673759182
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-11 18:50:01 -0300

    exercicio-q5

--- 03-BackEnd/b3/d2/exercicio.sql
+++ 03-BackEnd/b3/d2/exercicio.sql
@@ -34,3 +34,8 @@ ORDER BY t.name ASC;
 -- Exercício 5: Utilizando o RIGHT JOIN, faça uma busca que retorne todos os dados dos filmes, 
 -- mesmo os que não estão em cartaz e, adicionalmente, os dados dos cinemas que possuem estes 
 -- filmes em cartaz. Retorne os nomes dos cinemas em ordem alfabética.
+
+SELECT t.id, t.name, t.location, m.title, m.director, m.year, m.length_minutes
+FROM theater AS t
+RIGHT JOIN movies AS m ON t.id = m.theater_id
+ORDER BY t.name ASC;
