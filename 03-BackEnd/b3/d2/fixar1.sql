89d4765f89e835361581992d940517d8d208cac9
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-11 17:08:29 -0300

    fixar1-q6

--- 03-BackEnd/b3/d2/fixar1.sql
+++ 03-BackEnd/b3/d2/fixar1.sql
@@ -48,3 +48,14 @@ ON c.customer_id = r.customer_id
 WHERE c.active = 1
 GROUP BY c.customer_id
 ORDER BY c.first_name DESC, c.last_name DESC;
+
+-- Monte uma query que exiba o nome, sobrenome e a média de valor (amount) 
+-- paga aos funcionários no ano de 2006. Use as tabelas payment e staff. 
+-- Os resultados devem estar agrupados pelo nome e sobrenome do funcionário.
+
+SELECT s.first_name, s.last_name, AVG(p.amount) AS media
+FROM sakila.staff AS s
+INNER JOIN sakila.payment AS p
+ON s.staff_id = p.staff_id
+WHERE YEAR(p.payment_date) = 2006
+GROUP BY s.first_name, s.last_name;
