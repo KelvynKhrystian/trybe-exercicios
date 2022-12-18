8320550d9caf16734b0db6ce5ef0ef65d46f200d
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-18 16:44:56 -0300

    add condições

--- 03-BackEnd/b4/d1Runtime2/bmi.js
+++ 03-BackEnd/b4/d1Runtime2/bmi.js
@@ -10,6 +10,16 @@ const bmi = () => {
   const bmi = result.toFixed(2);
 
   console.log(`O seu BMI é ${bmi}`);
+  
+  // condições
+  if (bmi < 18.5) console.log('Abaixo do peso (magreza)'); 
+  if (bmi > 18.5 && bmi < 24.9) console.log('Peso normal');
+  if (bmi > 25 && bmi < 29.9) console.log('Acima do peso (sobrepeso)');
+  if (bmi > 30 && bmi < 34.9) console.log('Obesidade grau I');
+  if (bmi > 35 && bmi < 39.9) console.log('Obesidade grau II');
+  if (bmi > 40) console.log('Obesidade graus III e IV');
+
+  
   // return bmi; 
 }
 
