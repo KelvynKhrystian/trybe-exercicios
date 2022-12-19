484e5a054694cfa4c7cfb531e454954d3c836bf5
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-19 01:43:01 -0300

    exercicio-4

--- 03-BackEnd/b4/d2/Exercicios/src/app.js
+++ 03-BackEnd/b4/d2/Exercicios/src/app.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const app = express();
 
+const fs = require('fs').promises;
+const path = require('path');
+
+const diretorio = path.resolve(__dirname, './movies.json');
+
+const movies = async () => {
+  try {
+    const data = await fs.readFile(diretorio);
+    return JSON.parse(data);
+  } catch (erro) {
+    console.error(`Arquivo não pôde ser lido: ${error}`);
+  }
+}
+
+movies();
+
 module.exports = app;
