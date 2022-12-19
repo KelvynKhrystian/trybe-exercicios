fdf4ae6a67d78b04e7ba6a365c56b38a134b914b
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-12-19 01:24:47 -0300

    exercicio1

---
+++ 03-BackEnd/b4/d2/Exercicios/exercicios.js
@@ -0,0 +1,11 @@
+// Exercício 1
+// Qual é a função do código de status 400 e 422?
+
+// 400 Bad Request
+// Essa resposta significa que o servidor não entendeu 
+// a requisição pois está com uma sintaxe inválida.
+
+// 422 Unprocessable Entity (WebDAV (en-US))
+// A requisição está bem formada mas inabilitada para 
+// ser seguida devido a erros semânticos.
+
