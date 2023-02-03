2c09669962872caef364c310460cf5a7a578ac6e
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-02-03 19:47:31 -0300

    refatorando

--- 03-BackEnd/b4/d5/trybecash-api/src/server.js
+++ 03-BackEnd/b4/d5/trybecash-api/src/server.js
@@ -1,15 +1,17 @@
 // src/server.js
 const app = require('./app');
-const connection = require('./db/connection');
+// const connection = require('./db/connection');
 
 const PORT = 3001;
 
 app.listen(PORT, async () => {
   console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
 
-  // O código abaixo é para testarmos a comunicação com o MySQL
-  const [result] = await connection.execute('SELECT 1');
-  if (result) {
-    console.log('MySQL connection OK');
-  }
+//   // O código abaixo é para testarmos a comunicação com o MySQL
+//   const [result] = await connection.execute('SELECT 1');
+//   if (result) {
+//     console.log('MySQL connection OK');
+//   }
 });
+
+// o código comentado serve para ver se a conexão com o mysql ta ok, depois não é mais necessário
