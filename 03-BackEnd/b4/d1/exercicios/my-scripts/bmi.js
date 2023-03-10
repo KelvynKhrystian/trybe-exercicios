e72d75cb376d26840bed05385dc0f0a9fc33b21f
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-03-10 08:30:16 -0300

    ex4

--- 03-BackEnd/b4/d1/exercicios/my-scripts/bmi.js
+++ 03-BackEnd/b4/d1/exercicios/my-scripts/bmi.js
@@ -1,7 +1,7 @@
 const readline = require('readline-sync')
 
-const weight = readline.questionInt('Qual o seu peso ? ')
-const height = readline.questionInt('Qual a sua altura ? ')
+const weight = readline.questionFloat('Qual o seu peso ? ')
+const height = readline.questionFloat('Qual a sua altura ? ')
 
 const imc = weight / ( height * 2)
 
