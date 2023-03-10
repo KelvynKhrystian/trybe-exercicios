1928750bbdcfedd25a7a55d8aa389d86b6926632
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-03-10 17:31:13 -0300

    ex6-2

--- 03-BackEnd/b4/d1/exercicios/simpsons/index.js
+++ 03-BackEnd/b4/d1/exercicios/simpsons/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs').promises;
+const readline = require('readline-sync')
 
-const main = async () => {
+const simpsonsAll = async () => {
   try {
     const data = await fs.readFile('./simpsons.json', 'utf-8');
     const result = JSON.parse(data)
@@ -13,4 +14,24 @@ const main = async () => {
   }
 }
 
-main()
+const simpsonsId = async () => {
+  try {
+    const data = await fs.readFile('./simpsons.json', 'utf-8');
+    const result = JSON.parse(data)
+    const id = readline.question('Qual é o ID ? ')
+    const personID = await result.find((person) => id === person.id)
+    console.log(personID.name);
+
+  } catch (err) {
+    return `ID não encontrado`;
+  }
+}
+
+const main = () => {
+
+  // simpsonsAll()
+  simpsonsId()
+}
+
+
+main();
