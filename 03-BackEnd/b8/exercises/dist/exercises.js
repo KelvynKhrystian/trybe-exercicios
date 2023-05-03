55cd39445f57f38a8792194c086e95eb031c6357
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-05-03 18:45:37 -0300

    primeiro exercicio TS

---
+++ 03-BackEnd/b8/exercises/dist/exercises.js
@@ -0,0 +1,30 @@
+"use strict";
+// // ./exercises.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getLosangleArea = void 0;
+// export function getSquareArea(side: number): number {
+//   return side ** 2;
+// }
+// export function getRectangleArea(base: number, height: number): number {
+//   return base * height;
+// }
+// export function getTriangleArea(base: number, height: number): number {
+//   return (base * height) / 2;
+// }
+// export function getPolygonPerimeter(sides: number[]): number {
+//   return sides.reduce((acc, side) => acc + side, 0);
+// }
+// export function triangleCheck(
+//   sideA: number,
+//   sideB: number,
+//   sideC: number
+// ): boolean {
+//   const checkSideA = (sideB - sideC) < sideA && sideA < (sideB + sideC);
+//   const checkSideB = (sideA - sideC) < sideB && sideB < (sideA + sideC);
+//   const checkSideC = (sideA - sideB) < sideC && sideC < (sideA + sideB);
+//   return checkSideA && checkSideB && checkSideC;
+// }
+function getLosangleArea(dMaior, dMenor) {
+    return (dMaior * dMenor) / 2;
+}
+exports.getLosangleArea = getLosangleArea;
