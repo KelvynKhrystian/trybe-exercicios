d539de1783468c748daf6a13768b4f5bf8db6fd0
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-05-03 19:01:31 -0300

    req1-5/5

--- 03-BackEnd/b8/my-ts-scripts/ex1.ts
+++ 03-BackEnd/b8/my-ts-scripts/ex1.ts
@@ -34,3 +34,27 @@ const sortInput = (param: number[]) => {
 };
 
 console.log(sortInput(numberList));
+
+// Essa função é responsável por receber um objeto e formar uma frase utilizando as chaves do mesmo.
+const peopleInput = {
+  name: 'Rui',
+  age: 23,
+};
+
+const createSimpleSentence = (param: {name: string, age: number}) => {
+  return `Olá, meu nome é ${param.name} e tenho ${param.age} anos.`;
+}
+
+console.log(createSimpleSentence(peopleInput));
+
+
+// Essa é uma função que verifica se a idade passada é maior ou menor de 18 anos.
+const ageInput = 15;
+
+const isOfLegalAge = (param: string | boolean) => {
+  return !!param
+}
+
+ageInput >= 18
+  ? console.log(isOfLegalAge('true'))
+  : console.log(isOfLegalAge(false));
