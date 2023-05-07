a0468c4e8bf0155603ace70869af8426f7f59bb6
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-05-07 18:20:34 -0300

    ex3

--- 03-BackEnd/b8/my-ts-scripts/dist/mass.js
+++ 03-BackEnd/b8/my-ts-scripts/dist/mass.js
@@ -1,1 +1,10 @@
 "use strict";
+// ./mass.ts
+const units = ['kg', 'hg', 'dag', 'g', 'dg', 'cg', 'mg'];
+function convert2(value, fromUnit, toUnit) {
+    const fromIndex = units.indexOf(fromUnit);
+    const toIndex = units.indexOf(toUnit);
+    const exponent = toIndex - fromIndex;
+    return value * Math.pow(10, exponent);
+}
+console.log(convert2(15, 'g', 'kg'));
