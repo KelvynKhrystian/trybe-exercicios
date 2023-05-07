350e5463d6e3c1e1baf55354c6d592c3f141da80
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-05-07 18:47:26 -0300

    req7.1

--- 03-BackEnd/b8/my-ts-scripts/dist/length.js
+++ 03-BackEnd/b8/my-ts-scripts/dist/length.js
@@ -1,10 +1,15 @@
 "use strict";
+var readlineSync = require('readline-sync');
+var readValue = readlineSync.questionInt('Qual o valor da medida ? ');
+var readBase = readlineSync.question('Qual a medida atual ? ');
+var readConv = readlineSync.question('Para qual medida quer converter ? ');
 const medidas = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm'];
 const convert = (value, base, conv) => {
     const baseIndex = medidas.indexOf(base);
     const convIndex = medidas.indexOf(conv);
     const expoente = convIndex - baseIndex;
     const result = value * Math.pow(10, expoente);
-    return result;
+    const exec = `${value}${base} é igual a ${result}${conv} `;
+    return exec;
 };
-console.log(convert(15, 'm', 'km'));
+console.log(convert(readValue, readBase, readConv));
--- 03-BackEnd/b8/my-ts-scripts/length.ts
+++ 03-BackEnd/b8/my-ts-scripts/length.ts
@@ -1,3 +1,9 @@
+var readlineSync = require('readline-sync');
+
+var readValue: number = readlineSync.questionInt('Qual o valor da medida ? ');
+var readBase: string = readlineSync.question('Qual a medida atual ? ')
+var readConv: string = readlineSync.question('Para qual medida quer converter ? ');
+
 const medidas = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm'];
 
 const convert = (value: number, base: string, conv: string): number => {
@@ -5,8 +11,9 @@ const convert = (value: number, base: string, conv: string): number => {
   const baseIndex = medidas.indexOf(base);
   const convIndex = medidas.indexOf(conv);
   const expoente = convIndex - baseIndex;
-  const result = value * Math.pow(10, expoente)
-  return result
+  const result: number = value * Math.pow(10, expoente)
+  const exec: any = `${value}${base} é igual a ${result}${conv} `
+  return exec
 };
 
-console.log(convert(15,'m','km'))
+console.log(convert(readValue,readBase,readConv))
